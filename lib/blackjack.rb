9d68538e12140f25b3a99d282b3744d815db8d7b
Tiffany Nguyen <tiffanynuugen@gmail.com>
2018-08-15 02:11:58 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -26,7 +26,7 @@ def initial_round
   card_one = deal_card
   card_two = deal_card
   card_total = card_one + card_two
-  display_card_total(card_total) 
+  puts display_card_total(card_total) 
   return card_total
 end # code #initial_round here
 
