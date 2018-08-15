6b0850d7f57fc819cb81d5c2dc868010136b2012
Tiffany Nguyen <tiffanynuugen@gmail.com>
2018-08-15 02:25:20 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -1,65 +1,59 @@
 def welcome
+  # code #welcome here
   puts "Welcome to the Blackjack Table"
 end
-
-def deal_card
-  card = rand(1..11) #reuse the randomize method by setting it equal to a variable
+ def deal_card
+  # code #deal_card here
+  rand(1...12)
 end
-
+ def display_card_total
+  # code #display_card_total here
 def display_card_total(card_total)
   puts "Your cards add up to #{card_total}"
 end
-
-def prompt_user
+ def prompt_user
+  # code #prompt_user here
   puts "Type 'h' to hit or 's' to stay"
 end
-
-def get_user_input
-  user_input = gets.chomp
+ def get_user_input
+  # code #get_user_input here
+  gets.chomp
 end
-
+ def end_game
+  # code #end_game here
 def end_game(card_total)
   puts "Sorry, you hit #{card_total}. Thanks for playing!"
 end
-
-def initial_round
-  card_one = deal_card
-  card_two = deal_card
-  card_total = card_one + card_two
-  display_card_total(card_total) #once you have figured out the sum of your two cards, you can pass the total in as an argumnt so your phrase is displayed.
-  return card_total
+ def initial_round
+  # code #initial_round here
+  first_card = deal_card
+  second_card = deal_card
+  initial_round_total = first_card + second_card
+  display_card_total(initial_round_total)
+  initial_round_total
 end
-
+ def hit?
+  # code hit? here
 def hit?(card_total)
-  prompt_user #ask the user if they want to hit or stay
-  user_input = get_user_input #set a variable to use for your if/else statement
-  if user_input == "h"
-    card_total += deal_card #increase your former total by the new number given by dealing another card
-  elsif user_input == "s"
-    card_total #return the number and prompt user to hit again
-  else
-    invalid_command
-  end
-end
-
-def invalid_command
-  puts "Please enter a valid command"
   prompt_user
-  get_user_input
-end
-
-#####################################################
-# get every test to pass before coding runner below #
-#####################################################
-
-def runner
-  welcome
-  sum = initial_round
-  until sum > 21
-    sum = hit?(sum)
-    display_card_total(sum)
+  user_input = get_user_input
+  if user_input == "h"
+    new_card = deal_card
+    card_total += new_card
   end
-  end_game(sum)
+  card_total
 end
-
-runner
+ def invalid_command
+@@ -39,6 +51,11 @@ def invalid_command
+#####################################################
+ def runner
+  # code runner here
+  welcome
+  card_total = initial_round
+  until card_total > 21
+    card_total = hit?(card_total)
+    display_card_total(card_total)
+  end
+  end_game(card_total)
+end
+    
