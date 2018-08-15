f092e40c8c53160586ed6de8f3e99fbd5c6ec2d7
Tiffany Nguyen <tiffanynuugen@gmail.com>
2018-08-15 02:07:06 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -6,8 +6,7 @@ def deal_card
   rand(1...11)# code #deal_card here
 end
 
-def display_card_total(card_total)
-  card_total = 19
+def display_card_total
   puts "Your cards add up to #{card_total}" # code #display_card_total here
 end
 
